a96e53a4cfb21c00447510fb207f581de40bc04c
Puncoz Nepal <puncoz138@gmail.com>
2019-10-07 10:48:00 +0545

    refactoring test using different test database

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { importSchema } from "graphql-import"
 import { GraphQLServer } from "graphql-yoga"
 import { join } from "path"
 import "reflect-metadata"
-import { createConnection } from "typeorm"
 import { resolvers } from "./resolvers"
+import { createTypeOrmConnection } from "./utils/createTypeOrmConnection"
 
 export const startServer = async () => {
     const typeDefs = importSchema(join(__dirname, "./schema.graphql"))
 
     const server = new GraphQLServer({typeDefs, resolvers})
 
-    await createConnection()
+    await createTypeOrmConnection()
 
     const port = 4001
     await server.start({port})
--- src/test/register.test.ts
+++ src/test/register.test.ts
@@ -1,9 +1,13 @@
 import { request } from "graphql-request"
-import { createConnection } from "typeorm"
 import { User } from "../entity/User"
+import { createTypeOrmConnection } from "../utils/createTypeOrmConnection"
 import { host } from "./constants"
 
-const email = "newtest@jest.com"
+beforeAll(async () => {
+    await createTypeOrmConnection()
+})
+
+const email = "hello@world.com"
 const password = "secret"
 
 const mutation = `
@@ -12,14 +16,17 @@ mutation {
 }
 `
 
-test("Register user", async () => {
+test("Register user", async (done) => {
     const response = await request(host, mutation)
     expect(response).toEqual({register: true})
+    done()
 
-    await createConnection()
     const users = await User.find({where: {email}})
     expect(users).toHaveLength(1)
+
     const user = users[0]
     expect(user.email).toEqual(email)
     expect(user.password).not.toEqual(password)
+
+    done()
 })
